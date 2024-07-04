18149ac417f278f0ba178ac5d461b444e36a5d1e
Chiu_yeer <32241150+Chiuqyan@users.noreply.github.com>
2024-07-04 16:01:55 +0800

    Update main.py
    disable test

--- main.py
+++ main.py
@@ -10,7 +10,6 @@
 
 import json.decoder
 import os.path
-import os
 import shutil
 
 from gevent import monkey
@@ -204,12 +203,11 @@ class CoroutineSpeedup:
             # 将上下文替换成 Markdown 语法文本
             context: dict = self.channel.get()
             md_obj: dict = ot.to_markdown(context)
-            if not os.path.exists(SERVER_PATH_DOCS):
+            '''if not os.path.exists(SERVER_PATH_DOCS):
                 os.makedirs(SERVER_PATH_DOCS)
                 print(f"Directory {SERVER_PATH_DOCS} created.")
             else:
-                print(f"Directory {SERVER_PATH_DOCS} already exists.")
-
+                print(f"Directory {SERVER_PATH_DOCS} already exists.")'''
             # 子主题分流
             if not file_obj.get(md_obj["hook"]):
                 file_obj[md_obj["hook"]] = md_obj["hook"]
@@ -248,7 +246,7 @@ class CoroutineSpeedup:
             task = gevent.spawn(self._adaptor)
             task_list.append(task)
         gevent.joinall(task_list)
-        print(f"channel:{self.channel}")
+        #print(f"channel:{self.channel}")
 
 
 class _OverloadTasks:
